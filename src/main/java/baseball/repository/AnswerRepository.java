c7bd80629aafc5c02d1f78e3063588065a1eae4a
yeonjy <yeonjypaper@gmail.com>
2023-05-17 01:09:24 +0900

    feat: Repository 구현

---
+++ src/main/java/baseball/repository/AnswerRepository.java
@@ -0,0 +1,43 @@
+package baseball.repository;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswerRepository {
+
+    private List<Integer> answer;
+    private List<Integer> userAnswer;
+
+    public List<Integer> getAnswer() {
+        return answer;
+    }
+
+    public List<Integer> getUserAnswer() {
+        return userAnswer;
+    }
+
+    public List<Integer> makeAnswer() {
+        List<Integer> answer = new ArrayList<>();
+        while(answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        this.answer = answer;
+        return answer;
+    }
+
+    public List<Integer> setUerAnswer(String answer) {
+//        List<Integer> ints = Arrays.asList(Stream.of(answer.split("")).mapToInt(Integer::parseInt).toArray());
+        List<Integer> userAnswer = new ArrayList<Integer>();
+        String[] splitAnswer = answer.split("");
+        for(String tmp : splitAnswer) {
+            userAnswer.add(Integer.parseInt(tmp));
+        }
+        this.userAnswer = userAnswer;
+        return userAnswer;
+    }
+}
