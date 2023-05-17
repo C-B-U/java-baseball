0c4aba5bc63475d9aad7d373c6f412a5b3247c20
Heejung <ggy3r3@naver.com>
2023-05-17 13:29:38 +0900

    feat: main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import controller.Controller;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException{
         // TODO: 프로그램 구현
+        Controller controller = new Controller();
+        controller.startGame();
     }
 }
