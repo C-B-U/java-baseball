ef864f50199e12cb1f54e0c3aabb7e7a5a101ac3
yeonjy <yeonjypaper@gmail.com>
2023-05-17 11:57:08 +0900

    feat: 자동 반복

--- src/main/java/baseball/controller/InputController.java
+++ src/main/java/baseball/controller/InputController.java
@@ -3,27 +3,52 @@ package baseball.controller;
 import baseball.repository.AnswerRepository;
 import baseball.service.BaseballService;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class InputController {
-    private final AnswerRepository answerRepository = new AnswerRepository();
-    private final BaseballService baseballService = new BaseballService();
+
+    private static final String NOTHING = "낫싱";
+
+
+    private BaseballService baseballService = new BaseballService();
+    private AnswerRepository answerRepository = new AnswerRepository();
+
+    private static final String STRIKE = "스트라이크";
+
+    public void continueGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        int continueGame = Integer.parseInt(Console.readLine());
+        if (continueGame == 1) {
+            requireAnswer();
+        }
+        if (continueGame == 2) {
+            return;
+        }
+    }
 
     public void requireAnswer() {
-        answerRepository.makeAnswer();
+        baseballService.makeAnswer();
+        recieveUserAnswer();
     }
 
     public void recieveUserAnswer() {
-        String userAns = Console.readLine();
-        answerRepository.setUerAnswer(userAns);
+        System.out.print("숫자를 입력해주세요 : ");
+        String userAnswer = Console.readLine();
+        baseballService.makeUserAnswer(userAnswer);
+        checkCorrect();
     }
-    public String checkCorrect() {
-        return baseballService.makeReply(answerRepository.getAnswer(), answerRepository.getUserAnswer());
+
+
+    public void checkCorrect() {
+        Map<String, Integer> result = baseballService.makeReply(answerRepository.getAnswer(), answerRepository.getUserAnswer());
+        System.out.println(baseballService.makeStringReply(result));
+        if (result.containsKey(STRIKE) && result.get(STRIKE).equals(3)) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            continueGame();
+            return;
+        }
+        recieveUserAnswer();
     }
-
-
-
 }
