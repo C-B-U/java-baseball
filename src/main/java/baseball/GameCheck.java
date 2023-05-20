59f9bade201a43c785a3797f0d6fe784f55a61f2
heebum kim <heebum9955@naver.com>
2023-05-20 21:46:22 +0900

    feat: (GameCheck) add Enum class

---
+++ src/main/java/baseball/GameCheck.java
@@ -0,0 +1,17 @@
+package baseball;
+
+public enum GameCheck {
+    RESTART("1"),
+    QUIT("2"),
+    ;
+
+    private final String gameCheck;
+
+    GameCheck(String gameCheck) {
+        this.gameCheck = gameCheck;
+    }
+
+    public String getGameCheck() {
+        return gameCheck;
+    }
+}
