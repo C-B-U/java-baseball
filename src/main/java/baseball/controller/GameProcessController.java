31a683c794fd80f2845ed13b3f539dc12e1a6e56
yeonjy <yeonjypaper@gmail.com>
2023-05-24 07:12:03 +0900

    refactor: controller 클래스 분리

---
+++ src/main/java/baseball/controller/GameProcessController.java
@@ -0,0 +1,47 @@
+package baseball.controller;
+
+import baseball.PlayResult;
+import baseball.service.BaseballService;
+import baseball.service.StringReplyService;
+
+import java.util.Map;
+
+import static baseball.PlayResult.STRIKE;
+
+public class GameProcessController {
+    private final InputController inputController;
+    private final BaseballService baseballService = new BaseballService();
+    private final StringReplyService stringReplyService = new StringReplyService();
+
+    public GameProcessController(InputController inputController) {
+        this.inputController = inputController;
+    }
+
+    public void continueGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        if (inputController.isContinueGame()) {
+            makeGameAnswer();
+        }
+    }
+
+    public void makeGameAnswer() {
+        baseballService.makeAnswer();
+        requireUserAnswer();
+    }
+
+    public void requireUserAnswer() {
+        inputController.recieveUserAnswer(baseballService);
+        checkCorrect();
+    }
+
+    public void checkCorrect() {
+        Map<PlayResult, Integer> result = baseballService.makeReply();
+        System.out.println(stringReplyService.makeStringReply(result));
+        if (result.containsKey(STRIKE) && result.get(STRIKE).equals(3)) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            continueGame();
+            return;
+        }
+        requireUserAnswer();
+    }
+}
--- src/main/java/baseball/controller/InputController.java
+++ src/main/java/baseball/controller/InputController.java
@@ -1,54 +1,22 @@
 package baseball.controller;
 
-import baseball.repository.AnswerRepository;
+import baseball.exception.ControllerException;
 import baseball.service.BaseballService;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-import java.util.Map;
-
 public class InputController {
 
-    private static final String NOTHING = "낫싱";
+    final ControllerException controllerException = new ControllerException();
 
-
-    private BaseballService baseballService = new BaseballService();
-    private AnswerRepository answerRepository = new AnswerRepository();
-
-    private static final String STRIKE = "스트라이크";
-
-    public void continueGame() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-        int continueGame = Integer.parseInt(Console.readLine());
-        if (continueGame == 1) {
-            requireAnswer();
-        }
-        if (continueGame == 2) {
-            return;
-        }
-    }
-
-    public void requireAnswer() {
-        baseballService.makeAnswer();
-        recieveUserAnswer();
-    }
-
-    public void recieveUserAnswer() {
+    public void recieveUserAnswer(BaseballService baseballService) {
         System.out.print("숫자를 입력해주세요 : ");
         String userAnswer = Console.readLine();
         baseballService.makeUserAnswer(userAnswer);
-        checkCorrect();
     }
 
-
-    public void checkCorrect() {
-        Map<String, Integer> result = baseballService.makeReply(answerRepository.getAnswer(), answerRepository.getUserAnswer());
-        System.out.println(baseballService.makeStringReply(result));
-        if (result.containsKey(STRIKE) && result.get(STRIKE).equals(3)) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            continueGame();
-            return;
-        }
-        recieveUserAnswer();
+    public boolean isContinueGame() {
+        int continueGame = Integer.parseInt(Console.readLine());
+        controllerException.rightResponse(continueGame);
+        return continueGame == 1;
     }
 }
