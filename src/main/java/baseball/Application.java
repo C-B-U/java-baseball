af1b850d338bc1604d37645340b8ddfe0c244d1f
yeonjy <yeonjypaper@gmail.com>
2023-05-24 07:18:13 +0900

    refactor: 분리 controller 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,12 @@
 package baseball;
 
+import baseball.controller.GameProcessController;
 import baseball.controller.InputController;
 
 public class Application {
     public static void main(String[] args) {
-        InputController inputController = new InputController();
+        GameProcessController gameProcessController = new GameProcessController(new InputController());
         System.out.println("숫자 야구 게임을 시작합니다.");
-        inputController.requireAnswer();
+        gameProcessController.makeGameAnswer();
     }
 }
