4b5799664739e116c14ecebadb2e5fef432c3ded
yeonjy <yeonjypaper@gmail.com>
2023-05-24 07:11:20 +0900

    feat: service 예외 처리

---
+++ src/main/java/baseball/exception/ServiceException.java
@@ -0,0 +1,24 @@
+package baseball.exception;
+
+public class ServiceException {
+
+    public void correctUserAnswer(String userAnswer) {
+        lengthOfUserAnswer(userAnswer);
+        integerValue(userAnswer);
+    }
+
+    private void lengthOfUserAnswer(String userAnswer) {
+        if (userAnswer.length() != 3) {
+            throw new IllegalArgumentException("숫자는 3개만 입력해주세요.");
+        }
+    }
+
+    private void integerValue(String userAnswer) {
+        try {
+            Integer.parseInt(userAnswer);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
