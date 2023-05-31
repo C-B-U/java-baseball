b3cf6ae526169e45c7126bfe0996fa3edb02d8e1
Jiyoon Kim <rlawldbs0911@naver.com>
2023-05-31 22:28:39 +0900

    Feat : 게임 진행 메시지 enum 작성

---
+++ src/main/java/baseball/message/GameMessage.java
@@ -0,0 +1,17 @@
+package baseball.message;
+
+public enum GameMessage {
+    START_MESSAGE("숫자 야구 게임을 시작합니다."),
+    FINISH_MESSAGE("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    INPUT_MESSAGE("숫자를 입력해주세요 : ");
+
+    private String message;
+
+    GameMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
