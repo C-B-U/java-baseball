cdf8f8dfb0e647a3e3e4915c711797cf3806c597
GaBaljaintheroom <junsu1222@naver.com>
2023-07-28 22:26:35 +0900

    feat: 숫자야구 정답 시 결과 형식 추가

--- src/main/java/baseball/computer/service/ComputerService.java
+++ src/main/java/baseball/computer/service/ComputerService.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public class ComputerService {
 
+    private final static String THREE_STRIKE = "3스트라이크";
+    private final static String THREE_STRIKE_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final static String START_GAME_OR_STOP_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final ComputerRepository computerRepository;
     private final Message message;
 
@@ -23,6 +26,15 @@ public class ComputerService {
         StrikeAndBallDTO strikeAndBall = computerRepository.getStrikeAndBallDTO(userNumber);
         String strikeAndBallToString = message.toString(strikeAndBall.getStrike(), strikeAndBall.getBall());
         System.out.println(strikeAndBallToString);
+        checkCorrectNumber(strikeAndBallToString);
+    }
+
+    private void checkCorrectNumber(String strikeAndBallToString){
+        if (strikeAndBallToString.equals(THREE_STRIKE)){
+            System.out.println(THREE_STRIKE_GAME_OVER);
+            System.out.println(START_GAME_OR_STOP_GAME);
+        }
+
     }
 
 }
