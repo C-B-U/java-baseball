c56df0c9148b16fa777f6962ca6a51bc84a572f1
GaBaljaintheroom <junsu1222@naver.com>
2023-07-28 22:41:45 +0900

    refactor: 숫자야구 재시작 기능 수정

--- src/main/java/baseball/computer/controller/GameStart.java
+++ src/main/java/baseball/computer/controller/GameStart.java
@@ -3,24 +3,39 @@ package baseball.computer.controller;
 import baseball.computer.service.ComputerService;
 import baseball.user.controller.User;
 import baseball.user.service.UserService;
-
+import camp.nextstep.edu.missionutils.Console;
 public class GameStart {
-
+    private final static String THREE_STRIKE = "3스트라이크";
+    private final static String THREE_STRIKE_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final static String START_GAME_OR_STOP_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final ComputerService computerService;
 
     public GameStart (ComputerService computerService){
         this.computerService = computerService;
     }
 
-    User user = new User(new UserService());
     public void gameStart(){
         computerService.makeRandomNumber();
+        User user = new User(new UserService());
         System.out.println("숫자 야구 게임을 시작합니다.");
-
-        while (true){
-            computerService.getNumberResult(user.userNumber());
+        boolean startGame = true;
+        while (startGame){
+            String strikeAndBallToString = computerService.getNumberResult(user.userNumber());
+            startGame = checkCorrectNumber(strikeAndBallToString);
         }
 
+    }
+
+    private boolean checkCorrectNumber(String strikeAndBallToString){
+        if (strikeAndBallToString.equals(THREE_STRIKE)){
+            System.out.println(THREE_STRIKE_GAME_OVER);
+            System.out.println(START_GAME_OR_STOP_GAME);
+        }
+        return isRestartGame(Console.readLine());
+    }
+
+    private boolean isRestartGame(String userStatus){
+        return userStatus.equals("1");
     }
 
 
--- src/main/java/baseball/computer/service/ComputerService.java
+++ src/main/java/baseball/computer/service/ComputerService.java
@@ -7,9 +7,6 @@ import java.util.List;
 
 public class ComputerService {
 
-    private final static String THREE_STRIKE = "3스트라이크";
-    private final static String THREE_STRIKE_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private final static String START_GAME_OR_STOP_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final ComputerRepository computerRepository;
     private final Message message;
 
@@ -22,19 +19,11 @@ public class ComputerService {
         computerRepository.makeRandomNumber();
     }
 
-    public void getNumberResult(List<Integer> userNumber){
+    public String getNumberResult(List<Integer> userNumber){
         StrikeAndBallDTO strikeAndBall = computerRepository.getStrikeAndBallDTO(userNumber);
-        String strikeAndBallToString = message.toString(strikeAndBall.getStrike(), strikeAndBall.getBall());
-        System.out.println(strikeAndBallToString);
-        checkCorrectNumber(strikeAndBallToString);
+        return message.toString(strikeAndBall.getStrike(), strikeAndBall.getBall());
     }
 
-    private void checkCorrectNumber(String strikeAndBallToString){
-        if (strikeAndBallToString.equals(THREE_STRIKE)){
-            System.out.println(THREE_STRIKE_GAME_OVER);
-            System.out.println(START_GAME_OR_STOP_GAME);
-        }
 
-    }
 
 }
