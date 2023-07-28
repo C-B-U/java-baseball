5fe114078b93d780efdfc6732e20859433e33243
GaBaljaintheroom <junsu1222@naver.com>
2023-07-28 15:59:38 +0900

    feat: strike,ball 판별 Repository 작성

---
+++ src/main/java/baseball/computer/repository/ComputerRepository.java
@@ -0,0 +1,47 @@
+package baseball.computer.repository;
+
+import baseball.computer.dto.StrikeAndBallDTO;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class ComputerRepository {
+
+    private List<Integer> computerNumber;
+    private Long strike;
+    private Long ball;
+
+    public StrikeAndBallDTO getStrikeAndBallDTO(List<Integer> userNumber){
+        checkBaseballStrike(userNumber);
+        checkBaseballBall(userNumber);
+        return new StrikeAndBallDTO(strike, ball);
+    }
+
+    public void makeRandomNumber() {
+        computerNumber = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    }
+
+    public void checkBaseballStrike(List<Integer> userNumber){
+        strike = IntStream.range(0, userNumber.size())
+                .filter(i -> isStrike(userNumber.get(i), computerNumber.get(i)))
+                .count();
+    }
+
+    private boolean isStrike(int userNumber, int computerNumber) {
+        return userNumber == computerNumber;
+    }
+
+    public void checkBaseballBall(List<Integer> userNumber){
+        ball = IntStream.range(0, userNumber.size())
+                .filter(isBall(userNumber))
+                .count();
+    }
+
+    private IntPredicate isBall(List<Integer> userNumber) {
+        return i -> computerNumber.contains(userNumber.get(i))
+                && !isStrike(userNumber.get(i), computerNumber.get(i));
+    }
+
+}
