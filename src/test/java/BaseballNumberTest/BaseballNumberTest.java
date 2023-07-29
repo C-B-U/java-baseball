61b9db7dc1bee2debaccfc264dde50747d741ab1
GaBaljaintheroom <junsu1222@naver.com>
2023-07-29 23:28:52 +0900

    Test: 입력 값 List<Integer>변환 Test

--- src/test/java/BaseballNumberTest/BaseballNumberTest.java
+++ src/test/java/BaseballNumberTest/BaseballNumberTest.java
@@ -2,8 +2,11 @@ package BaseballNumberTest;
 
 
 import baseball.user.exeption.InputValidator;
+import baseball.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -44,6 +47,22 @@ public class BaseballNumberTest {
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> inputValidator.validateUserNumberCorrect("109"));
         assertEquals(CHECK_USER_NUMBER, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("String 문자열을 List<Integer>로 변환")
+    void userNameStringToListInteger(){
+        //given
+        UserService userService = new UserService();
+        String numString = "123";
+        List<Integer> numListInteger = List.of(1,2,3);
+
+        //when
+        List<Integer> result = userService.userNumberIntegerList(numString);
+
+        //then
+        assertThat(result).isEqualTo(numListInteger);
+
     }
 
 
