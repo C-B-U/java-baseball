c2f6d6e5ec3280a2cffc0cba005f189da56e7765
김승진 <ohksj77@gmail.com>
2023-07-29 18:54:53 +0900

    feat: add enum
    Add new enum
    - game message
    - random number range

---
+++ src/main/java/baseball/RandomNumberRange.java
@@ -0,0 +1,17 @@
+package baseball;
+
+public enum RandomNumberRange {
+    MIN(1),
+    MAX(9),
+    CIPHER(3);
+
+    private final Integer num;
+
+    RandomNumberRange(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+}
