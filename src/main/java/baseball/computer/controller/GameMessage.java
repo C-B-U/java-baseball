b2a463416f91448deba47dbdd6f394c2cc52f743
GaBaljaintheroom <junsu1222@naver.com>
2023-08-01 00:59:19 +0900

    refactor: 게임 메시지 추가

--- src/main/java/baseball/computer/controller/GameMessage.java
+++ src/main/java/baseball/computer/controller/GameMessage.java
@@ -1,6 +1,7 @@
 package baseball.computer.controller;
 
 public enum GameMessage {
+    START_GAME("숫자 야구 게임을 시작합니다."),
     THREE_STRIKE("3스트라이크"),
     THREE_STRIKE_GAME_OVER("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
     START_GAME_OR_STOP_GAME("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
