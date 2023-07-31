fbe1b2fe1540cdcfbd1280f656f9aaee0438598f
Jiyoon Kim <rlawldbs0911@naver.com>
2023-07-31 15:11:36 +0900

    feat: Computer class

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,50 +5,45 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import static baseball.message.NumberMessage.*;
 
 public class Computer {
     private static final String BALL = "볼";
     private static final String STRIKE = "스트라이크";
     private static final String NONE = "낫싱";
 
-    List<Integer> computerNumber = new ArrayList<>();
-    public void generateRandomNumber(){
-
-        while (computerNumber.size() < LENGTH_OF_QUIZ.getNumber()) {
-            int randomNumber = Randoms.pickNumberInRange(MINIMUM_NUMBER.getNumber(), MAXIMUM_NUMBER.getNumber());
-            if (!computerNumber.contains(randomNumber)) {
-                computerNumber.add(randomNumber);
-            }
-        }
+    List<Integer> computerNumber;
+    public Computer() {
+        computerNumber = makeRandomNumber();
         System.out.println(computerNumber);
     }
 
-    public String compareWithUserNumber(String number) {
+    private List<Integer> makeRandomNumber() {
+        List<Integer> numberList = new ArrayList<>();
+        do {
+            int newNumber = Randoms.pickNumberInRange(1, 9);
+            if (numberList.contains(newNumber))
+                continue;
+            numberList.add(newNumber);
+        } while (numberList.size() < 3);
+
+        return numberList;
+    }
+
+    public String compareWithUserNumber(List<Integer> number) {
         int ball = 0;
         int strike = 0;
-        List<Integer> userNumber = userNumberToList(number);
 
-        for (int i = 0; i < userNumber.size(); i++) {
-            if (Objects.equals(userNumber.get(i), computerNumber.get(i))) {
+        for (int i = 0; i < number.size(); i++) {
+            if (Objects.equals(number.get(i), computerNumber.get(i))) {
                 strike++;
                 continue;
             }
 
-            if (computerNumber.contains(userNumber.get(i))) {
+            if (computerNumber.contains(number.get(i))) {
                 ball++;
             }
         }
-
         return makeString(ball, strike);
-    }
-
-    private List<Integer> userNumberToList(String userNumber) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int length = 0; length < userNumber.length(); length++) {
-            numbers.add(Integer.parseInt(String.valueOf(userNumber.charAt(length))));
-        }
-        return numbers;
     }
 
     private String makeString(int ball, int strike) {
