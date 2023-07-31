11e32f80280f29758fcce82149f59484410fa403
Jiyoon Kim <rlawldbs0911@naver.com>
2023-07-31 15:11:06 +0900

    feat: Application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,12 +2,10 @@ package baseball;
 
 
 public class Application {
-
-    Computer computer = new Computer();
     public static void main(String[] args) {
         // 게임 시작
-        BaseballGame baseball = new BaseballGame();
-        baseball.gameStart();
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.startGame();
 
     }
 }
